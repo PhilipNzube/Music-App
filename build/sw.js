ea52db9882784bbae4b1b1e17f7f4698de5b236f
PhilipNzube <72675495+PhilipNzube@users.noreply.github.com>
2022-12-27 11:33:06 +0100

    Add files via upload

--- build/sw.js
+++ build/sw.js
@@ -23,8 +23,8 @@ this.addEventListener("install", (event) => {
                 '/static/media/Trippie%20Red%20Cover.43875b64aa09aee79ccd.jpg',
                 '/static/media/NoUserIcon.cafab3219dab5d0a44ae.png',
                 'static/media/PauseButton.dc0699633a71f4eee247.png',
-                '/static/js/main.34de1caf.js',
-                '/static/css/main.f4d9b464.css',
+                '/static/js/main.a750fb0b.js',
+                '/static/css/main.a86d0a69.css',
                 '/ICON.png',
                 '/ICON192.png',
                 '/ICON512.png',
@@ -35,7 +35,8 @@ this.addEventListener("install", (event) => {
     )
 })
 this.addEventListener("fetch", (event) => {
-     //if (!navigator.onLine) {
+    if (!navigator.onLine) {
+        console.log("OFFLINE FUCKERS!!!");
         event.respondWith(
             caches.match(event.request).then((resp) => {
                 if (resp) {
@@ -45,5 +46,7 @@ this.addEventListener("fetch", (event) => {
 
 
         )
-    // }
+    } else {
+        console.log("ONLINE FUCKERS!!!");
+    }
 })
